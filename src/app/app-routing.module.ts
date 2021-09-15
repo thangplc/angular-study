9d2c79cedc15a0fb29668e5edcee6013352c31c0
thangplc <thangplc1412@gmail.com>
2021-09-15 18:54:28 +0700

    update img

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Practice2Component } from './practice2/practice2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: Practice2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
